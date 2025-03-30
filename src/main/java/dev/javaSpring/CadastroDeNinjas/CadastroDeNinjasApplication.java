1727e9c69da49f8eb302a9c2dbb673b8b497e879
Glaudencio12 <glaudenciocosta015@gmail.com>
2025-03-30 08:15:20 -0300

    :tada: Commit inicial

---
+++ src/main/java/dev/javaSpring/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -0,0 +1,11 @@
+package dev.javaSpring.CadastroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjasApplication.class, args);
+	}
+}
